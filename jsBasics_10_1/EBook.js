266b233e998c643784dfc6c20009cf16ef9745cf
Viktor H <zenderska@gmail.com>
2026-02-03 17:10:30 +0200

    HM10_add_new_folder_and_files

---
+++ jsBasics_10_1/EBook.js
@@ -0,0 +1,34 @@
+import Book from "./Book.js";
+
+class EBook extends Book {
+  constructor(title, author, year, fileFormat) {
+    super(title, author, year);
+    this.fileFormat = fileFormat;
+  }
+
+  // getter
+  get fileFormat() {
+    return this._fileFormat;
+  }
+
+  // setter з валідацією
+  set fileFormat(value) {
+    if (typeof value !== "string" || value.trim() === "") {
+      throw new Error("File format must be a string");
+    }
+    this._fileFormat = value;
+  }
+
+  printInfo() {
+    console.log(
+      `Title: "${this.title}", Author: ${this.author}, Year: ${this.year}, File format: ${this.fileFormat}`
+    );
+  }
+
+  // ===== 4.3 статичний метод =====
+  static fromBook(book, fileFormat) {
+    return new EBook(book.title, book.author, book.year, fileFormat);
+  }
+}
+
+export default EBook;
---
+++ jsBasics_10_1/task1.js
@@ -0,0 +1,34 @@
+import Book from "./Book.js";
+import EBook from "./EBook.js";
+
+// 2. Створення об'єктів Book
+const book1 = new Book("The Secret History", "Donna Tartt", 1993);
+const book2 = new Book("1984", "George Orwell", 1949);
+const book3 = new Book("Fahrenheit 451", "Ray Bradbury", 1953);
+const book4 = new Book("Harry Potter and the Chamber of Secrets", "J.K. Rowling", 1998);
+
+book1.printInfo();
+book2.printInfo();
+book3.printInfo();
+book4.printInfo();
+
+// 3. Створення об'єкта EBook
+const ebook1 = new EBook("Clean Code", "Robert C. Martin", 2008, "PDF");
+const ebook2 = new EBook("The Shining", "Stephen King", 1977, "PDF");
+ebook1.printInfo();
+ebook2.printInfo();
+
+// 4.1 Використання геттерів / сеттерів
+ebook1.fileFormat = "EPUB";
+console.log("New File format:", ebook1.fileFormat);
+
+// 4.2 Масив книг + пошук найдавнішої
+const books = [book1, book2, book3, book4, ebook1, ebook2];
+const oldestBook = Book.getOldestBook(books);
+
+console.log("Oldest Book:");
+oldestBook.printInfo();
+
+// 4.3 Створення EBook з Book
+const newEBook = EBook.fromBook(book4, "FB2");
+newEBook.printInfo();
